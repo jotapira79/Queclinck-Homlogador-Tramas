046363e9e25bc9345dc6aa5c68109b1b47811aa1
jotapira79 <jotapira@gmail.com>
2025-10-09 11:46:41 -0300

    feat(ingestor): generic SQLite ingestor by report (GTINF, GTERI, future-proof)

--- src/ingestors/__init__.py
+++ src/ingestors/__init__.py
@@ -1,5 +1,5 @@
 """Módulos de ingesta para homologador Queclink."""
 
-from .sqlite_gtinf import ensure_db, ingest_lines  # noqa: F401
+from .sqlite_records import ensure_db, ingest_lines  # noqa: F401
 
 __all__ = ["ensure_db", "ingest_lines"]
